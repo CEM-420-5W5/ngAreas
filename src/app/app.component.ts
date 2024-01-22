885ab109e9186cc0049081542ef0d80fd3e0166f
Mathieu Briau <mathieu.briau@gmail.com>
2024-01-21 21:13:47 -0500

    Affichage de base des images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngAreas';
+  apiUrl = "https://localhost:7269/";
+  cats:any[] = [];
+
+  constructor(public http: HttpClient) { }
+
+  async ngOnInit(){
+    this.cats = await lastValueFrom(this.http.get<any>(this.apiUrl+'api/Cats'));
+    console.log(this.cats);
+  }
 }
